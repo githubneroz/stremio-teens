226f8a7c0273af66f38cfa2f6b44f8f3192db2d1
o-informante <110304940+o-informante@users.noreply.github.com>
2023-05-07 22:19:44 -0300

    Update editar.js

--- pop-up/js/editar.js
+++ pop-up/js/editar.js
@@ -6,7 +6,7 @@
             '<span></span>' +
             '<a href="https://www.instagram.com/p/B2X-1Z_gGZA/?utm_source=ig_web_copy_link" target="_blank" class="bio_btn">Participar do Sorteio &nbsp; <i class="fa fa-instagram"></i></a>' +
             '</div>',
-            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: #fafafa; text-align: center;background-image: url(pop-up/imagens-pop/sorteio04.png);background-size: cover;}' +
+            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: #fafafa; text-align: center;background-image: url(pop-up/imagens-pop/sorteio04.jpg);background-size: cover;}' +
             '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
             '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
             '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
